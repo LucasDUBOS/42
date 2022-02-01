064cecbd0a43ee0835d68e4d169f6996dfd413b8
Lucas DUBOS <lucasdubos@gmail.com>
2022-02-01 20:57:09 +0100

    Delete bootcamp_python directory

--- bootcamp_python/day00/ex04/operations.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if len(sys.argv) != 3 or not sys.argv[1].isnumeric() or not sys.argv[2].isnumeric():
-    print("InputError: must be 2 numbers")
-    sys.exit(0)
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-print("sum:        ", a + b,"\nDifference: ",a - b ,"\nProduct:    ",a * b,"\nQuotient:   ",a / b,"\nRemainder:  ",a % b)
